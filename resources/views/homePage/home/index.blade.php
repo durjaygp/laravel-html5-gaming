73cd4d0c4d162e97af3c59f8a92c8ec835810f69
Durjay Ghosh <42237029+durjaygp@users.noreply.github.com>
2023-12-27 00:58:10 +0600

    update

--- resources/views/homePage/home/index.blade.php
+++ resources/views/homePage/home/index.blade.php
@@ -3,26 +3,50 @@
     Home Page
 @endsection
 @section('content')
-    <!-- slider-area -->
-    <section class="slider-area slider-bg">
+    <!-- banner-area -->
+    @php
+        $blogs = \App\Models\Blog::latest()->whereStatus(1)->take(5)->get();
+    @endphp
+    <section class="home-six-banner">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-9">
-                    <div class="slider-content text-center">
-                        <h6 class="wow fadeInDown" data-wow-delay=".2s">islands</h6>
-                        <h2 class="tlt fix" data-in-effect="fadeInLeft">hunter <span>killer</span></h2>
-                        <p class="wow fadeInUp" data-wow-delay="2s">Find technology or people for digital projects in public sector and Find an individual specialist develope researcher.</p>
-                        <a href="#" class="btn wow fadeInUp" data-wow-delay="2.2s">READ MORE</a>
+            <div class="row">
+                <div class="col-12">
+                    <div class="banner-content">
+                        <h2 class="title-one">gaming</h2>
+                        <h3 class="title-two">world class <span>Blog</span></h3>
+                    </div>
+                    <div class="banner-news-heading">
+                        <div class="headline">
+                            <h5>Latest <span>News</span></h5>
+                        </div>
+                        <div class="social-link">
+                            <ul>
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="banner-magazine-active">
+                        @foreach($blogs as $row)
+                        <div class="banner-magazine-item red">
+                            <div class="banner-magazine-thumb">
+                                <img src="{{asset($row->image)}}" alt="">
+                            </div>
+                            <div class="banner-magazine-content">
+                                <span class="date">{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->format('M d, Y h:i A')}}</span>
+                                <h5 class="title"><a href="{{route('home.blogDetails',$row->slug)}}">{{$row->name}}</a></h5>
+                            </div>
+                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <!-- slider-area-end -->
-
+    <!-- banner-area-end -->
     <!-- area-bg-one -->
     <div class="area-bg-one">
-
         <!-- latest-games-area -->
         <section class="latest-games-area pt-120">
             <div class="container">
@@ -56,105 +80,40 @@
             </div>
         </section>
         <!-- latest-games-area-end -->
-
-        <!-- about-us-area -->
-        <section class="about-us-area pt-90 pb-120">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 order-0 order-lg-2">
-                        <div class="about-img">
-                            <img src="{{asset('geco')}}/img/images/about_us_img.jpg" alt="">
-                            <a href="https://www.youtube.com/watch?v=_LTiEXMc5J0" class="popup-video"><img src="{{asset('geco')}}/img/icon/play_btn.png"  alt=""></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="section-title title-style-two mb-45">
-                            <span>about story</span>
-                            <h2>Noob Dia <span>Paradiso</span> Tournament</h2>
-                        </div>
-                        <div class="about-content">
-                            <p>Find technology or people for digital projects in public sector and Find an individual specialist develo researcher.</p>
-                            <p>Consectur adipiscing elit, sed do eiusmod tempor incididunt labore dolore mana aliqua. Unimd minim venam quis nostrud
-                                exercitation ullamco laboris nisi ut aliquip commodo cpsam.</p>
-                            <div class="about-btn">
-                                <a href="#" class="btn">READ MORE</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- about-us-area-end -->
-
     </div>
     <!-- area-bg-one-end -->
     <!-- blog-area -->
+    @php
+    $blogs = \App\Models\Blog::latest()->whereStatus(1)->take(3)->get();
+    @endphp
     <section class="blog-area pt-115 pb-120">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-xl-6 col-lg-8">
                     <div class="section-title title-style-three text-center mb-70">
                         <h2>Latest News <span>Articles</span></h2>
-                        <p>Compete with 100 players on a remote island for winner takes showdown
-                            known issue where certain skin strategic</p>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="blog-post blog-style-two mb-50">
-                        <div class="blog-thumb mb-30">
-                            <a href="#"><img src="{{asset('geco')}}/img/blog/s_blog_thumb01.jpg" alt=""></a>
-                        </div>
-                        <div class="blog-post-content">
-                            <h4><a href="#">Shooter action video game</a></h4>
-                            <div class="blog-meta">
-                                <ul>
-                                    <li><i class="far fa-clock"></i>July 4, 2020</li>
-                                    <li><i class="fas fa-tag"></i><a href="#">Shooter</a></li>
-                                </ul>
+                @foreach($blogs as $row)
+                    <div class="col-lg-4 col-md-6">
+                        <div class="blog-post blog-style-two mb-50">
+                            <div class="blog-thumb mb-30">
+                                <a href="{{route('home.blogDetails',$row->slug)}}"><img src="{{asset($row->image)}}" alt=""></a>
                             </div>
-                            <p>Compete with 100 players on a remote island for winner takes showdown known for issue where
-                                certain skin strategic [...]</p>
+                            <div class="blog-post-content">
+                                <h4><a href="{{route('home.blogDetails',$row->slug)}}">{{$row->name}}</a></h4>
+                                <div class="blog-meta">
+                                    <ul>
+                                        <li><i class="far fa-clock"></i>{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->format('M d, Y h:i A')}}</li>
+                                    </ul>
+                                </div>
+                                <p>{{\Illuminate\Support\Str::limit($row->description, 90)}}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="blog-post blog-style-two mb-50">
-                        <div class="blog-thumb mb-30">
-                            <a href="#"><img src="{{asset('geco')}}/img/blog/s_blog_thumb02.jpg" alt=""></a>
-                        </div>
-                        <div class="blog-post-content">
-                            <h4><a href="#">multiplayer environments</a></h4>
-                            <div class="blog-meta">
-                                <ul>
-                                    <li><i class="far fa-clock"></i>July 4, 2020</li>
-                                    <li><i class="fas fa-tag"></i><a href="#">Shooter</a></li>
-                                </ul>
-                            </div>
-                            <p>Compete with 100 players on a remote island for winner takes showdown known for issue where
-                                certain skin strategic [...]</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="blog-post blog-style-two mb-50">
-                        <div class="blog-thumb mb-30">
-                            <a href="#"><img src="{{asset('geco')}}/img/blog/s_blog_thumb03.jpg" alt=""></a>
-                        </div>
-                        <div class="blog-post-content">
-                            <h4><a href="#">Bullet Force Multiplayer</a></h4>
-                            <div class="blog-meta">
-                                <ul>
-                                    <li><i class="far fa-clock"></i>July 4, 2020</li>
-                                    <li><i class="fas fa-tag"></i><a href="#">Shooter</a></li>
-                                </ul>
-                            </div>
-                            <p>Compete with 100 players on a remote island for winner takes showdown known for issue where
-                                certain skin strategic [...]</p>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </section>
--- resources/views/homePage/inc/header.blade.php
+++ resources/views/homePage/inc/header.blade.php
@@ -1,42 +1,4 @@
 <header>
-    <div class="header-top-area d-none d-lg-block">
-        <div class="container-fluid container-full-padding">
-            <div class="row align-items-center">
-                <div class="col-lg-6 d-none d-lg-block">
-                    <div class="header-top-offer">
-                        <p>Exclusive Black Friday ! Offer</p>
-                        <span class="coming-time" data-countdown="2020/6/20"></span>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="header-top-right">
-                        <div class="header-social">
-                            <ul>
-                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
-                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="header-top-action">
-                            <ul>
-                                <li>
-                                    <div class="header-top-mail">
-                                        <p><span>|</span><i class="far fa-envelope"></i><a href="https://themebeyond.com/cdn-cgi/l/email-protection#c4adaaa2ab84a3a9a5ada8eaa7aba9"><span class="__cf_email__" data-cfemail="f59c9b939ab59290969a9c9b939adb969a98">[email&#160;protected]</span></a></p>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="header-user-login">
-                                        <a href="#"><i class="fas fa-user"></i>Login</a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <div id="sticky-header" class="main-header">
         <div class="container-fluid container-full-padding">
             <div class="row">
@@ -48,34 +10,11 @@
                             </div>
                             <div id="mobile-menu" class="navbar-wrap d-none d-lg-flex">
                                 <ul>
-                                    <li class="show"><a href="#">Home</a>
-                                        <ul class="submenu">
-                                            <li><a href="index-2.html">Home One</a></li>
-                                            <li><a href="index-3.html">Home Two</a></li>
-                                            <li><a href="index-4.html">Home Three</a></li>
-                                            <li class="active"><a href="index.html">Home Four</a></li>
-                                            <li><a href="index-5.html">Home Five</a></li>
-                                            <li><a href="index-6.html">Home Six</a></li>
-                                            <li><a href="index-7.html">Home Seven</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="#">Pages</a>
-                                        <ul class="submenu">
-                                            <li><a href="about-us.html">About Story</a></li>
-                                            <li><a href="upcoming-games.html">Upcoming Games</a></li>
-                                            <li><a href="game-single.html">Game Single</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="game-overview.html">Overview</a></li>
-                                    <li><a href="community.html">Community</a></li>
-                                    <li><a href="shop.html">Store</a></li>
-                                    <li><a href="#">Blog</a>
-                                        <ul class="submenu">
-                                            <li><a href="blog.html">News Page</a></li>
-                                            <li><a href="blog-details.html">News Details</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="contact.html">contact</a></li>
+                                    <li class="show"><a href="{{route('home')}}">Home</a></li>
+                                    <li><a href="game-overview.html">About Us</a></li>
+                                    <li><a href="#">Games</a></li>
+                                    <li><a href="game-overview.html">Blog</a></li>
+                                    <li><a href="community.html">Contact Us</a></li>
                                 </ul>
                             </div>
                             <div class="header-action">
